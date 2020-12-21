fb96090c7242ba1706f2090de3a63977d4e765d4
ltth <43105481+ltth@users.noreply.github.com>
2020-12-21 18:45:00 +0700

    Update java8.sh

--- java8.sh
+++ java8.sh
@@ -1,3 +1,32 @@
 export JAVA_HOME=/usr/lib/jvm/jre-openjdk
 export PATH=$PATH:$JAVA_HOME/bin
 export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar
+
+###################################################
+----------config filebeat push log to logstash----------
+
+filebeat.inputs:
+- type: log
+  enabled: true
+  paths:
+#    - /opt/waf/logs/*
+    - /var/log/*.log
+output.logstash:
+ # codec.format:
+  #  string: '%{[message]}'
+  hosts: ["127.0.0.1:5044"]
+  
+ ----------config filebeat push log to kafka---------- 
+ 
+#output.kafka:
+ # codec.format:
+  #  string: '%{[message]}'
+  #hosts: ["192.168.253.224:9092"]
+  #topic: filebeat
+  #enable: true
+  #partition.round_robin:
+   # reachable_only: false
+  #required_acks: 1
+  #compression: gzip
+  #max_message_bytes: 1000000
+
